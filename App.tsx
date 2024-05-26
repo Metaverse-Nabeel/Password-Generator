a57c72dc71c6bf095a2df40e4664833b7499ace7
Nabeel Ahmed <nabeelahmed3129@gmail.com>
2024-05-26 21:58:39 +0500

    Logic for the password generator

--- App.tsx
+++ App.tsx
@@ -1,12 +1,74 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function App() {
+//Form validation 
+import * as Yup from 'yup'
+
+const PasswordScehma = Yup.object().shape({
+  passwordLength: Yup.number()
+    .min(4, 'Should be a minimum of four characters')
+    .max(16, 'Should be a maximum of sixteen characters')
+    .required('Length is required')
+})
+
+const App = () => {
+  const [password, setPassword] = useState('')
+  const [isPassGenerated, setIsPassGenerated] = useState(false)
+  const [lowerCase, setLowerCase] = useState(true)
+  const [upperCase, setUpperCase] = useState(false)
+  const [numbers, setNumbers] = useState(false)
+  const [symbols, setSymbols] = useState(false)
+
+  const gereratePasswordString = (passwordLength: number) => {
+    let charactersList = '';
+    const upperCaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const lowerCaseChars = 'abcdefghijklmnopqrstuvwxyz';
+    const digitChars = '0123456789';
+    const specialChars = '!@#$%^&*()_-+=';
+
+    if (upperCase) {
+      charactersList += upperCaseChars;
+    }
+    if (lowerCase) {
+      charactersList += lowerCaseChars;
+    }
+    if (numbers) {
+      charactersList += digitChars;
+    }
+    if (symbols) {
+      charactersList += specialChars;
+    }
+
+    const passwordResult = createPassword(charactersList, passwordLength);
+    setPassword(passwordResult);
+    setIsPassGenerated(true);
+  }
+
+  const createPassword = (characters: string, passwordLength: number) => {
+    let result = ''
+    for (let i = 0; i < passwordLength; i++) {
+      const characterIndex = Math.round(Math.random() * characters.length)
+      result += characters.charAt(characterIndex);
+    }
+    return result;
+  }
+
+  const resetPassword = () => {
+    setPassword('');
+    setIsPassGenerated(false);
+    setUpperCase(false);
+    setLowerCase(true);
+    setNumbers(false);
+    setSymbols(false);
+  }
+
   return (
     <View>
-      <Text className='text-center text-2xl font-bold text-[#000]'>Password Generator App</Text>
+      <Text className='my-2 text-center text-2xl font-bold text-[#000]'>Password Generator App</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({})
+
+export default App
